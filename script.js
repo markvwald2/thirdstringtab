d567cb3b01abe1cf15ec572d2426fd9d3082770a
Mark Vahrenwald <markvahrenwald@Marks-MacBook-Air.local>
2025-02-11 20:04:52 -0700

    refined random button functionality

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-$(function(){
-    const treeView = $("#simple-treeview").dxTreeView({ 
+$(function() {
+    const treeView = $("#simple-treeview").dxTreeView({
         items: bands,
         width: 300,
         searchEnabled: true,
         onItemClick: function(data) {
             var item = data.node;
-            if(data.itemData.url) {
+            if (data.itemData.url) {
                 $("#band-details").removeClass("hidden");
                 $("#band-details > .name").text(item.parent.text + " - " + item.text);
                 $("#band-details > object").attr("data", data.itemData.url);
@@ -16,10 +16,12 @@ $(function(){
     }).dxTreeView("instance");
 
     // Add random song functionality
-    $('#random').on('click', function() {
+    $(document).on('click', '#random', function() {
+        console.log("Random button clicked!");
+
         // Collect all songs from all bands
         let allSongs = [];
-        
+
         bands.forEach(band => {
             band.items.forEach(song => {
                 allSongs.push({
@@ -28,16 +30,20 @@ $(function(){
                 });
             });
         });
-        
+
         // Select a random song
         const randomEntry = allSongs[Math.floor(Math.random() * allSongs.length)];
-        
-        // Update the display
+
+        // Reset the object data before updating
+        $("#band-details > object").attr("data", "");
+
+        // Update the display with the random song
+        console.log("Random song:", randomEntry.song.text);
         $("#band-details").removeClass("hidden");
         $("#band-details > .name").text(randomEntry.bandName + " - " + randomEntry.song.text);
         $("#band-details > object").attr("data", randomEntry.song.url);
     });
-    
+
     // Style the random button
     $('#random').css({
         'cursor': 'pointer',
@@ -49,7 +55,11 @@ $(function(){
         'display': 'inline-block',
         'user-select': 'none'
     }).hover(
-        function() { $(this).css('background-color', '#45a049'); },
-        function() { $(this).css('background-color', '#4CAF50'); }
+        function() {
+            $(this).css('background-color', '#45a049');
+        },
+        function() {
+            $(this).css('background-color', '#4CAF50');
+        }
     );
 });
